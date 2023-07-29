37a4e754fada22d7e590e89f23ce9e91209b3879
Caetano Smaniotto <smaniottocaetano@gmail.com>
2023-07-29 15:35:25 -0300

    Initial

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_session import Session
+
+app = Flask(__name__)
+app.secret_key = 'lol'
+
+# Configuração da extensão Flask-Session
+app.config['SESSION_TYPE'] = 'filesystem'  # Ou outro método de armazenamento de sessão de sua escolha
+Session(app)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    # session.clear()
+
+    if 'carrinho' not in session:
+        session["carrinho"] = {}
+
+    if request.method == 'POST':
+
+        if (not request.form["produto"]) or (not float(request.form["quantidade"])) or (float(request.form["quantidade"]) <= 0):
+            return redirect(url_for("index"))
+        
+        produto = request.form["produto"].lower()
+        quantidade = float(request.form["quantidade"])
+
+        if produto in session["carrinho"]:
+            session["carrinho"][produto] += quantidade
+        else:
+            session["carrinho"][produto] = quantidade
+
+        # print(session["carrinho"])
+        return render_template("index.html")
+
+    else:
+        return render_template("index.html")
+
+@app.route("/remover/<produto>")
+def remover(produto):
+
+    del session["carrinho"][produto]
+
+    return redirect(url_for("index"))
+
+@app.route("/checkout")
+def checkout():
+    session.clear()
+
+    return redirect(url_for("index"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
